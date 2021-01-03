52e177c1ff5f1c32c02fa4673487a83995fbc35f
Mateusz Pietrzak <simcitymrsuperslid@gmail.com>
2021-01-03 22:06:12 +0100

    Get data from BT module by phone

--- app/src/main/java/com/example/gardenos/ControlActivity.kt
+++ app/src/main/java/com/example/gardenos/ControlActivity.kt
@@ -3,23 +3,26 @@ package com.example.gardenos
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
-import android.os.Bundle
-import android.os.Handler
+import android.os.*
 import android.util.Log
-import android.view.VerifiedInputEvent
+import android.util.Log.d
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.HandlerCompat
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.android.synthetic.main.control_activity.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
-import java.io.IOError
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
+import java.io.*
+import java.lang.NullPointerException
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 import java.util.*
+import kotlin.random.Random
 
 
 class ControlActivity : AppCompatActivity() {
@@ -35,12 +38,12 @@ class ControlActivity : AppCompatActivity() {
         lateinit var mBluetoothAdapter: BluetoothAdapter
         lateinit var mAddress: String
         lateinit var mName: String
+        lateinit var mmInStream: InputStream
        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.control_activity)
-
         connectProgressBar.visibility = View.INVISIBLE
 
         mName = intent.getStringExtra(MainActivity.extraName).toString()
@@ -52,26 +55,49 @@ class ControlActivity : AppCompatActivity() {
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         mBluetoothDevice = mBluetoothAdapter.getRemoteDevice(mAddress)
 
+
         connectButton.setOnClickListener {
             if (!mIsConnected) {
                 CoroutineScope(IO).launch {
                     connect()
                 }
             }
+        }
 
+        testButton.setOnClickListener {
+            send("a")
+            //send("test")
+            CoroutineScope(IO).launch {
+                readData()
+            }
         }
 
         dscButton.setOnClickListener {
             disconnect()
         }
 
-        testButton.setOnClickListener {
-            send("a")
-            send("test")
-        }
-
     }
 
+    private suspend fun readData() {
+        val mmBuffer = ByteArray(1024)
+
+        if (mIsConnected) {
+            coroutineScope {
+                var numBytes: Int
+                while (mIsConnected) run {
+                    try {
+                        numBytes = mmInStream.read(mmBuffer)
+                        runOnUiThread(java.lang.Runnable {
+                            outputText.append(mmBuffer.decodeToString(endIndex = numBytes))
+                        })
+                    } catch (e: IOException) {
+                        Log.e("IOE", e.toString())
+                        this.cancel()
+                    }
+                }
+            }
+        }
+        }
 
     private fun send(input: String) {
         if (mIsConnected) {
@@ -110,6 +136,7 @@ class ControlActivity : AppCompatActivity() {
             runOnUiThread(java.lang.Runnable {
                 if(mBluetoothSocket!!.isConnected) {
                     mIsConnected = true
+                    mmInStream = mBluetoothSocket!!.inputStream
                 }
                 connectProgressBar.visibility = View.INVISIBLE
                 btConnected.visibility = View.VISIBLE
@@ -133,7 +160,6 @@ class ControlActivity : AppCompatActivity() {
             try {
                 mBluetoothSocket!!.close()
                 mBluetoothSocket = null
-
             } catch (e: IOException) {
                 Toast.makeText(this, "Coś poszło nie tak", Toast.LENGTH_SHORT).show()
                 e.printStackTrace()
@@ -150,5 +176,6 @@ class ControlActivity : AppCompatActivity() {
             }
         }
     }
+
 }
 
